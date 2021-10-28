66c9e06bac6f8d3894592dc64f3fe025af509970
Alex Turtu <turtu.alexandru@gmail.com>
2021-10-28 16:04:30 +0300

    Still need to add palms and make sure palms and coins appear at different places

--- personalproject2710/jscript.js
+++ personalproject2710/jscript.js
@@ -1,95 +1,113 @@
 var i = 100;
 var j = 50;
-var palmTop = 100 ;
+var palmTop = 100;
 var plamLfet = 50;
-var coinTop = 0;
-var coinLeft = 0;
+var palm1Top = 100;
 var score = 0;
-var selectArray = ["demo","demo1"];
-function startG(){
-   
-    palmTop = Math.floor(Math.random()*4);
-    palmLeft = Math.floor(Math.random()*7);
-    palmTop = palmTop * 100;
-    palmLeft = palmLeft * 100;
-    document.getElementById("palm1").style.top = palmTop +"px";
-    document.getElementById("palm1").style.left = palmLeft+"px";
-    randomCoin()
+var selectArray = ["demo", "demo1"];
+function startG() {
+  palmTop = Math.floor(Math.random() * 4);
+  palmLeft = Math.floor(Math.random() * 7);
+  palmTop = palmTop * 100;
+  palmLeft = palmLeft * 100;
+  document.getElementById("palm1").style.top = palmTop + "px";
+  document.getElementById("palm1").style.left = palmLeft + "px";
+  randomCoin();
 }
-function randomCoin(){
-    coinTop = Math.floor(Math.random()*4);
-    coinLeft = Math.floor(Math.random()*7);
-    coinTop = coinTop *50;
-    coinLeft = coinLeft *100;
-    document.getElementById("coin").style.left = coinLeft+"px";
-    document.getElementById("coin").style.top = coinTop +"px";
-    
+function randomCoin() {
+  coinTop = Math.floor(Math.random() * 4);
+  coinLeft = Math.floor(Math.random() * 7);
+  coinTop = coinTop * 50;
+  coinLeft = coinLeft * 100;
+  document.getElementById("coin").style.left = coinLeft + "px";
+  document.getElementById("coin").style.top = coinTop + "px";
 }
-function checkMatch(){
-    if (coinTop == j  && coinLeft == i ) {
-        score = score +100;
-       randomCoin()
-       startG()
-      document.getElementById("score").innerHTML = "SCORE: $" +score
-        
-    }
+function checkMatch() {
+  if (coinTop == j - 300 && coinLeft == i - 100) {
+    score = score + 100;
+    randomCoin();
+    startG();
+    document.getElementById("score").innerHTML = "SCORE: $" + score;
+  }
 }
-
 
 function mover() {
-    checkMatch()
-    if ( i!= palmLeft  || j !== palmTop  + 150 && j != palmTop + 100 ){
-        while (i < 700 ){
-            
-            i = i + 100
-            document.getElementById("monster").style.left = i+"px";
-            document.getElementById("monster").style.right = i +"px";
-            return;
-        }
-    }else{
-            alert("Avoid the palm tree")
-        }
-    
-}
-function movel(){
-    checkMatch()
-    if ( i!= palmLeft +200  || j !== palmTop+100 && j != palmTop+150){
-            while (i > 100){
-                
-            i = i - 100
-            document.getElementById("monster").style.left = i+"px";
-            document.getElementById("monster").style.right = i +"px";
-            
-            return;
-        }
-    }else{
-        alert("Avoid the palm tree")
+  if (i != palmLeft || (j !== palmTop + 150 && j != palmTop + 100)) {
+    while (i < 700) {
+      i = i + 100;
+      document.getElementById("monster").style.left = i + "px";
+      document.getElementById("monster").style.right = i + "px";
+      checkMatch();
+      return;
     }
-}
-function moveu(){
-    checkMatch()
-    if (i!=palmLeft+100 || j != palmTop+200){
-    while(j>100){
-    j = j - 50
-    document.getElementById("monster").style.top = j+"px";
-    document.getElementById("monster").style.bottom = j+"px";
-    return;
+  } else {
+    alert("Ouuups!!! The Palm is a trap! Score lowered by $100.");
+    score = score - 100;
+    document.getElementById("score").innerHTML = "SCORE: $" + score;
+    startG();
+    if (score == -100) {
+      alert("! Game Over !");
+      location.reload();
     }
-}else{
-    alert("Avoid the palm tree")
+  }
 }
-}
-function moved(){
-    checkMatch()
-    if (i != palmLeft + 100 || j != palmTop+50 ){
-        while (j <500 ){
-        j = j + 50
-        document.getElementById("monster").style.top = j+"px";
-        document.getElementById("monster").style.bottom = j+"px";
-        return;
-        }
-    }else{
-        alert("Avoid the palm tree")
+function movel() {
+  if (i != palmLeft + 200 || (j !== palmTop + 100 && j != palmTop + 150)) {
+    while (i > 100) {
+      i = i - 100;
+      document.getElementById("monster").style.left = i + "px";
+      document.getElementById("monster").style.right = i + "px";
+      checkMatch();
+      return;
     }
+  } else {
+    alert("Ouuups!!! The Palm is a trap! Score lowered by $100.");
+    score = score - 100;
+    document.getElementById("score").innerHTML = "SCORE: $" + score;
+    startG();
+    if (score == -100) {
+      alert("! Game Over !");
+      location.reload();
+    }
+  }
 }
-
+function moveu() {
+  if (i != palmLeft + 100 || j != palmTop + 200) {
+    while (j > 100) {
+      j = j - 50;
+      document.getElementById("monster").style.top = j + "px";
+      document.getElementById("monster").style.bottom = j + "px";
+      checkMatch();
+      return;
+    }
+  } else {
+    alert("Ouuups!!! The Palm is a trap! Score lowered by $100.");
+    score = score - 100;
+    document.getElementById("score").innerHTML = "SCORE: $" + score;
+    startG();
+    if (score == -100) {
+      alert("! Game Over !");
+      location.reload();
+    }
+  }
+}
+function moved() {
+  if (i != palmLeft + 100 || j != palmTop + 50) {
+    while (j < 450) {
+      j = j + 50;
+      document.getElementById("monster").style.top = j + "px";
+      document.getElementById("monster").style.bottom = j + "px";
+      checkMatch();
+      return;
+    }
+  } else {
+    alert("Ouuups!!! The Palm is a trap! Score lowered by $100.");
+    score = score - 100;
+    document.getElementById("score").innerHTML = "SCORE: $" + score;
+    startG();
+    if (score == -100) {
+      alert("! Game Over !");
+      location.reload();
+    }
+  }
+}
