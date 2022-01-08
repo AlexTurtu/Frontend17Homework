a288c3332cdda5e17d9db753b0e1f6d5efc1fd18
AlexTurtu <turtu.alexandru@gmail.com>
2022-01-08 21:07:06 +0200

    added JS

--- Homework8/app.js
+++ Homework8/app.js
@@ -1,9 +1,10 @@
 var addCommentBtn = document.querySelector("#addComment");
 var commentID = document.querySelector("#commentID");
 var commentEmail = document.querySelector("#emailAddress");
-var commentsParagraph = document.querySelector("#commentParagraph");
+var commentsShow = document.querySelector(".commentsShow");
 var comment = document.querySelector("#textBoxComment");
 var id = 1000;
+var divCommClass = document.querySelectorAll(".divCommClass");
 class Comments {
   constructor(id, email, message) {
     this.id = id;
@@ -14,9 +15,34 @@ class Comments {
 const allComments = [];
 
 function createCommnent() {
-  id++;
-  console.log(id);
   const newComment = new Comments(id, commentEmail.value, comment.value);
-  allComments.push = newComment;
+  var divComm = document.createElement("div");
+  var commClose = document.createElement("div");
+  //create the comment box
+  commentsShow.appendChild(divComm);
+  divComm.id = id;
+  divComm.className = "divCommClass";
+  divComm.innerHTML = `<p class="commentBody">${comment.value} </p> </br> <p class="commentEmail">From: ${commentEmail.value}</p>`;
+  // create the close comment div
+  commClose.id = "ID" + id;
+  commClose.className = "commentCloseClass";
+  commClose.innerHTML = `<p onclick="deleteComment(${id})">Delete comment with id: #ID${id}...</p>`;
+  divComm.appendChild(commClose);
+  allComments.push(newComment);
   console.log(allComments);
+
+  id++;
 }
+function showHideComments() {
+  document.querySelector(".commentsShow").classList.toggle("isHidden");
+  document.querySelector(".showHide").classList.toggle("showHideBlack");
+}
+
+function deleteComment(idToDelete) {
+  document.getElementById(idToDelete).remove();
+  console.log(idToDelete);
+}
+function emailIsValid(emailToCheck) {
+  console.log("Ëmail validation");
+}
+document.getElementById("emailAddress").addEventListener("blur", emailIsValid);
